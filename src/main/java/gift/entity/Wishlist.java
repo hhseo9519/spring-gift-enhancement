f71dbd5adadfd06a2291df33d962c2bdc55abaed
hhseo9519 <hhseo9519@naver.com>
2025-07-15 16:10:20 +0900

    feat : 테이블의 외래키 매핑

--- src/main/java/gift/entity/Wishlist.java
+++ src/main/java/gift/entity/Wishlist.java
@@ -2,9 +2,11 @@ package gift.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,9 +16,11 @@ public class Wishlist {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "member_id", nullable = false)
     private Long memberId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "product_id", nullable = false)
     private Long productId;
 
