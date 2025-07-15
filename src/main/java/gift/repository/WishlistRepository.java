7e183766776ac4508bfa617e306c5f41180e8421
hhseo9519 <hhseo9519@naver.com>
2025-07-15 16:00:34 +0900

    feat : JPA에 맞는 Repository로 변경

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -1,103 +1,16 @@
 package gift.repository;
 
 import gift.entity.Wishlist;
-import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
-@Repository
-public class WishlistRepository {
-    private final JdbcClient jdbcClient;
+    List<Wishlist> findByMemberId(Long memberId);
 
-    public WishlistRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+    Optional<Wishlist> findByMemberIdAndProductId(Long memberId, Long productId);
 
-    public void save(Wishlist wishlist) {
-        jdbcClient.sql("INSERT INTO wishlist (member_id, product_id, quantity) " +
-                        "VALUES (:memberId, :productId, :quantity)")
-                .param("memberId", wishlist.getMemberId())
-                .param("productId", wishlist.getProductId())
-                .param("quantity", wishlist.getQuantity())
-                .update();
-    }
-
-
-    public List<Wishlist> findByMemberId(Long memberId) {
-        return jdbcClient.sql(
-                        "SELECT id, member_id, product_id, quantity " +
-                                "  FROM wishlist " +
-                                " WHERE member_id = :memberId")
-                .param("memberId", memberId)
-                .query((rs, rowNum) -> mapRow(rs))
-                .list();
-    }
-
-
-    public Optional<Wishlist> findByMemberIdAndProductId(Long memberId, Long productId) {
-        return jdbcClient.sql(
-                        "SELECT id, member_id, product_id, quantity " +
-                                "  FROM wishlist " +
-                                " WHERE member_id = :memberId " +
-                                "   AND product_id = :productId")
-                .param("memberId", memberId)
-                .param("productId", productId)
-                .query((rs, rowNum) -> mapRow(rs))
-                .optional();
-    }
-
-
-    public void deleteByMemberIdAndProductId(Long memberId, Long productId) {
-        jdbcClient.sql("DELETE FROM wishlist WHERE member_id = :memberId AND product_id = :productId")
-                .param("memberId", memberId)
-                .param("productId", productId)
-                .update();
-    }
-
-    private Wishlist mapRow(ResultSet rs) throws SQLException {
-        return new Wishlist(
-                rs.getLong("id"),
-                rs.getLong("member_id"),
-                rs.getLong("product_id"),
-                rs.getInt("quantity")
-        );
-    }
-
-    public void update(Wishlist wishlist) {
-        jdbcClient.sql("UPDATE wishlist SET quantity = :quantity WHERE member_id = :memberId AND product_id = :productId")
-                .param("memberId", wishlist.getMemberId())
-                .param("productId", wishlist.getProductId())
-                .param("quantity", wishlist.getQuantity())
-                .update();
-    }
-    public void upsertWishlist(Long memberId, Long productId) {
-        jdbcClient.sql("""
-        MERGE INTO wishlist (member_id, product_id, quantity)
-        KEY (member_id, product_id)
-        VALUES (
-          :memberId,
-          :productId,
-          COALESCE(
-            (SELECT quantity
-               FROM wishlist
-              WHERE member_id = :memberId
-                AND product_id = :productId
-            ) + 1,
-            1
-          )
-        )
-        """)
-                .param("memberId", memberId)
-                .param("productId", productId)
-                .update();
-    }
-
-
-
+    void deleteByMemberIdAndProductId(Long memberId, Long productId);
 }
