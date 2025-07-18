85313e2906dfb7c16e5a08f4b2a592975ddd7fa9
hhseo9519 <hhseo9519@naver.com>
2025-07-18 12:17:58 +0900

    feat : product 페이지네이션 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -6,6 +6,10 @@ import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,10 +30,10 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-
     @GetMapping
-    public ResponseEntity<List<ProductResponseDto>> findAllProduct() {
-        return new ResponseEntity<>(productService.findAllProduct(), HttpStatus.OK);
+    public ResponseEntity<Page<ProductResponseDto>> findAllProduct(
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(productService.findAllProduct(pageable));
     }
 
 
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -6,6 +6,8 @@ import gift.entity.Product;
 import gift.repository.ProductRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,12 +37,13 @@ public class ProductService {
 
     }
 
-    public List<ProductResponseDto> findAllProduct() {
-        return productRepository.findAll().stream()
 
-                .map(this::toDto)
-                .toList();
+
+    public Page<ProductResponseDto> findAllProduct(Pageable pageable) {
+        return productRepository.findAll(pageable)
+                .map(this::toDto);
     }
+
 
     public ProductResponseDto addProduct(ProductRequestDto productRequestDto) {
 
